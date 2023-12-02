6d5689fab86a7473e094bed426c8673bfc020597
baykirac <kiracmehmetburkay@gmail.com>
2023-12-03 00:24:49 +0300

    P005 Solved

---
+++ Project Euler/Problem Solutions/P005.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Euler.Problem_Solutions
+{
+    internal class P005
+    {
+        public static int smallest_Multipe()
+        {
+            int number = 1;
+            bool isSmallest = false;
+            while (true)
+            {
+                for(int i = 1 ; i <= 20; i++)
+                {
+                    if(number % i != 0)
+                    {
+                        isSmallest = false;
+                        break;
+                    }
+                    else
+                        isSmallest = true;
+                }
+                if (isSmallest)
+                    return number;
+                number++;
+            }
+        }
+    }
+}
